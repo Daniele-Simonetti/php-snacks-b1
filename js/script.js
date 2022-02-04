6f4d1ea6443fd536a43419369d5efb4ada840ce5
Daniele-Simonetti <daniele.simonetti99@outlook.it>
2022-02-04 17:00:58 +0100

    add array

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 const App = new Vue (
   {
     el: '#app',
+    data: {
+      carsAvailable: [],
+    },
     created() {
       axios.get('http://localhost:8888/php-snacks-b1/server/controller.php')
         .then((result) => {
+          this.carsAvailable = result.data;
           console.log(result);
         })
           .catch((error) => {
